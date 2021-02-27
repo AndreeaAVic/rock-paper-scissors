1a7d8fa6f1669d32d3a36d4183bd68b931e0125a
AndreeaAVic <andreeahidigan6@gmail.com>
2021-02-27 22:11:12 +0200

    add classes to user's choice

--- index.js
+++ index.js
@@ -4,7 +4,71 @@ const playBtn = document.querySelector('#play-btn');
 const valueInput = document.querySelector('#value-input');
 const userChoiceResult = document.querySelector('#user-choice-span');
 const computerChoiceResult = document.querySelector('#computer-choice-span');
-const result = document.querySelector('#result'); 
+const result = document.querySelector('#result');
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const rockName = document.querySelector('#choice-rock');
+const paperName = document.querySelector('#choice-paper');
+const scissorsName = document.querySelector('#choice-scissors');
+
+
+function play() {
+    rock.addEventListener('click', function() {
+        this.classList.add('choice--selected');
+        paper.classList.remove('choice--selected');
+        scissors.classList.remove('choice--selected');
+        rockName.classList.add('choice-name--selected');
+        paperName.classList.remove('choice-name--selected');
+        scissorsName.classList.remove('choice-name--selected');
+        clickOnImage('rock');
+    });
+    
+    paper.addEventListener('click', function() {
+        this.classList.add('choice--selected');
+        rock.classList.remove('choice--selected');
+        scissors.classList.remove('choice--selected');
+        paperName.classList.add('choice-name--selected');
+        rockName.classList.remove('choice-name--selected');
+        scissorsName.classList.remove('choice-name--selected');
+        clickOnImage('paper');
+    });
+    
+    scissors.addEventListener('click', function() {
+        this.classList.add('choice--selected');
+        rock.classList.remove('choice--selected');
+        paper.classList.remove('choice--selected');
+        scissorsName.classList.add('choice-name--selected');
+        rockName.classList.remove('choice-name--selected');
+        paperName.classList.remove('choice-name--selected');
+        clickOnImage('scissors');
+    });
+}
+
+play();
+
+
+function clickOnImage(choice) {
+    displayChoice(userChoiceResult, choice);
+
+    let computerChoice = getRandomComputerChoice();
+    displayChoice(computerChoiceResult, computerChoice);
+
+    let result = compareChoices(choice, computerChoice);
+    displayResult(result);
+}
+
+
+function getImageIndex(clickedImage) {
+    const choices = document.getElementsByTagName('img');
+    for (let i = 0; i < choices.length; i++) {
+        const choice = choices[i];
+
+        if (clickedImage === choice) {
+            return i;
+        }
+    }
+}
 
 
 function playGame() {
